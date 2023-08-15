e1e953d5433af4aca77a0496a42b60b96c5d70d4
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-08-15 18:42:49 +0200

    rename to Acceleration from RunSpeed

--- src/movement.rs
+++ src/movement.rs
@@ -4,15 +4,15 @@ use bevy_rapier3d::prelude::*;
 #[derive(Debug, Bundle)]
 pub struct MovementBundle {
     pub controller: Controller,
-    pub run_speed: RunSpeed,
+    pub acceleration: Acceleration,
     pub jump_height: JumpHeight,
 }
 
 impl MovementBundle {
-    pub fn new(run_speed: f32, jump_height: f32) -> Self {
+    pub fn new(acceleration: f32, jump_height: f32) -> Self {
         Self {
             controller: Controller::default(),
-            run_speed: RunSpeed(run_speed),
+            acceleration: Acceleration(acceleration),
             jump_height: JumpHeight(jump_height),
         }
     }
@@ -40,7 +40,7 @@ impl Default for Controller {
 }
 
 #[derive(Debug, Component)]
-pub struct RunSpeed(pub f32);
+pub struct Acceleration(pub f32);
 
 #[derive(Debug, Component)]
 pub struct JumpHeight(pub f32);
@@ -48,16 +48,17 @@ pub struct JumpHeight(pub f32);
 const MIN_VELOCITY: f32 = -4.;
 const MAX_VELOCITY: f32 = 4.;
 
-pub fn update_movement(mut query: Query<(&mut Velocity, &Controller, &RunSpeed, &JumpHeight)>) {
-    for (mut velocity, controller, run_speed, jump_height) in &mut query {
+pub fn update_movement(mut query: Query<(&mut Velocity, &Controller, &Acceleration, &JumpHeight)>) {
+    for (mut velocity, controller, acceleration, jump_height) in &mut query {
         let right: f32 = if controller.right_pressed { 1. } else { -1. };
         let left: f32 = if controller.left_pressed { 1. } else { -1. };
         let up: f32 = if controller.up_pressed { 1. } else { -1. };
         let down: f32 = if controller.down_pressed { 1. } else { -1. };
 
-        velocity.linvel.z += ((run_speed.0 / 100.) * (down - up)).clamp(MIN_VELOCITY, MAX_VELOCITY);
+        velocity.linvel.z +=
+            ((acceleration.0 / 100.) * (down - up)).clamp(MIN_VELOCITY, MAX_VELOCITY);
         velocity.linvel.x +=
-            ((run_speed.0 / 100.) * (right - left)).clamp(MIN_VELOCITY, MAX_VELOCITY);
+            ((acceleration.0 / 100.) * (right - left)).clamp(MIN_VELOCITY, MAX_VELOCITY);
 
         if controller.jump_just_pressed {
             velocity.linvel.y = jump_height.0;
